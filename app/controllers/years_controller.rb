9341390ba37d6b34b64c46b092e8f857bd8b2f3f
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-21 11:34:08 -0500

    Created Years controller, as well as routes to each year's view page and a dynamic _form

---
+++ app/controllers/years_controller.rb
@@ -0,0 +1,53 @@
+class YearsController < ApplicationController
+
+  def y_1990
+    @year = 1990
+
+  end
+
+  def y_1991
+    @year = 1991
+
+  end
+
+  def y_1992
+    @year = 1992
+
+  end
+
+  def y_1993
+    @year = 1993
+
+  end
+
+  def y_1994
+    @year = 1994
+
+  end
+
+  def y_1995
+    @year = 1995
+
+  end
+
+  def y_1996
+    @year = 1996
+
+  end
+
+  def y_1997
+    @year = 1997
+
+  end
+
+  def y_1998
+    @year = 1998
+
+  end
+
+  def y_1999
+    @year = 1999
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'application#home'
+
+  get '/1990', to: 'years#y_1990'
+  get '/1991', to: 'years#y_1991'
+  get '/1992', to: 'years#y_1992'
+  get '/1993', to: 'years#y_1993'
+  get '/1994', to: 'years#y_1994'
+  get '/1995', to: 'years#y_1995'
+  get '/1996', to: 'years#y_1996'
+  get '/1997', to: 'years#y_1997'
+  get '/1998', to: 'years#y_1998'
+  get '/1999', to: 'years#y_1999'
+
 end
