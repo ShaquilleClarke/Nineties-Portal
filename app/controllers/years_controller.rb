97d9d2bf5e7cd3dbcb6ed43a1168749fc7f28b84
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-01-22 16:52:39 -0500

    forms rendering dynamically for all years

--- app/controllers/years_controller.rb
+++ app/controllers/years_controller.rb
@@ -1,53 +1,52 @@
 class YearsController < ApplicationController
 
   def y_1990
-    @year = 1990
-
+    year_info_find(1990)
   end
 
   def y_1991
-    @year = 1991
-
+    year_info_find(1991)
   end
 
   def y_1992
-    @year = 1992
-
+    year_info_find(1992)
   end
 
   def y_1993
-    @year = 1993
-
+    year_info_find(1993)
   end
 
   def y_1994
-    @year = 1994
-
+    year_info_find(1994)
   end
 
   def y_1995
-    @year = 1995
-
+    year_info_find(1995)
   end
 
   def y_1996
-    @year = 1996
-
+    year_info_find(1996)
   end
 
   def y_1997
-    @year = 1997
-
+    year_info_find(1997)
   end
 
   def y_1998
-    @year = 1998
-
+    year_info_find(1998)
   end
 
   def y_1999
-    @year = 1999
-
+    year_info_find(1999)
   end
+
+  private
+
+    def year_info_find(year)
+      @year = year
+      @movies = Movie.where(release_year: year)
+      @songs = Song.where(release_year: year)
+      @games = Game.where(release_year: year)
+    end
 
 end
